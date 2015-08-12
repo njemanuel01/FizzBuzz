909f400fa1352515fb937dda07818436415689d0
Nicholas Emanuel <njemanuel01@gmail.com>
2015-08-12 12:49:10 -0500

    Added 3rd test case

--- FizzBuzz/Test.cs
+++ FizzBuzz/Test.cs
@@ -7,6 +7,7 @@ public class FizzBuzzTests
 {
 	[TestCase (1, "1")]
 	[TestCase (2, "2")]
+	[TestCase (3, "Fizz")]
 	public void Translate (int input, string expected)
 	{
 		string result = Translator.Translate (input);
