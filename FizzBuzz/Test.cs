97092ff6c2ebd8f6e13790bfa82f14c312bcbd7f
Nicholas Emanuel <njemanuel01@gmail.com>
2015-08-12 12:38:35 -0500

    Added cs file Created test for Translate method

---
+++ FizzBuzz/Test.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+
+
+[TestFixture ()]
+public class FizzBuzzTests
+{
+	[TestCase (1, "1")]
+	public void Translate (int input, string expected)
+	{
+		string result = Translator.Translate (input);
+		Assert.That(result, Is.EqualTo(expected));
+	}
+}
+
+public class Translator
+{
+	public static string Translate(int i)
+	{
+		return "1";
+	}
+}
+
