1bdd4ef3c277b15156cbdb8f5d5bd3acb78208fd
Nicholas Emanuel <njemanuel01@gmail.com>
2015-08-13 10:18:43 -0500

    Added methods to rps

--- FizzBuzz/RockPaperScissors.cs
+++ FizzBuzz/RockPaperScissors.cs
@@ -22,20 +22,41 @@ public class RockPaperScissors
 {
 	public static string winCondition(string weapon1, string weapon2)
 	{
-		if (weapon1 == "Rock" && weapon2 == "Scissors")
+		if (Player1Wins(weapon1, weapon2))
 			return "Player1";
-		if (weapon1 == "Paper" && weapon2 == "Rock")
-			return "Player1";
-		if (weapon1 == "Scissors" && weapon2 == "Paper")
-			return "Player1";
-		if (weapon1 == "Paper" && weapon2 == "Scissors")
+		if (Player2Wins(weapon1, weapon2))
 			return "Player2";
-		if (weapon1 == "Rock" && weapon2 == "Paper")
-			return "Player2";
-		if (weapon1 == "Scissors" && weapon2 == "Rock")
-			return "Player2";
-		if (weapon1 == "Rock" && weapon2 == "Rock")
+		if (Tie(weapon1, weapon2))
 			return "Tie";
 		return "No Game";
+	}
+
+	public static bool Player1Wins(string weapon1, string weapon2)
+	{
+		if (weapon1 == "Rock" && weapon2 == "Scissors")
+			return true;
+		if (weapon1 == "Paper" && weapon2 == "Rock")
+			return true;
+		if (weapon1 == "Scissors" && weapon2 == "Paper")
+			return true;
+		return false;
+	}
+
+	public static bool Player2Wins(string weapon1, string weapon2)
+	{
+		if (weapon1 == "Rock" && weapon2 == "Paper")
+			return true;
+		if (weapon1 == "Paper" && weapon2 == "Scissors")
+			return true;
+		if (weapon1 == "Scissors" && weapon2 == "Rock")
+			return true;
+		return false;
+	}
+
+	public static bool Tie(string weapon1, string weapon2)
+	{
+		if (weapon1 == weapon2)
+			return true;
+		return false;
 	}
 }
