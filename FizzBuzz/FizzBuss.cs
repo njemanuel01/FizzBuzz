d8d38eeae150fb6428ddaada687a916596314df3
Nicholas Emanuel <njemanuel01@gmail.com>
2015-08-13 09:59:37 -0500

    Added test file for RockPaperScissors

---
+++ FizzBuzz/FizzBuss.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+
+[TestFixture]
+public class FizzBuzzTests
+{
+	[TestCase (1, "1")]
+	[TestCase (2, "2")]
+	[TestCase (3, "Fizz")]
+	[TestCase (5, "Buzz")]
+	[TestCase (6, "Fizz")]
+	[TestCase (10, "Buzz")]
+	public void Translate(int input, string expected)
+	{
+		string result = Translator.Translate (input);
+		Assert.That (result, Is.EqualTo(expected));
+	}
+}
+
+public class Translator
+{
+	public static string Translate(int i)
+	{
+		if (i % 3 == 0)
+			return "Fizz";
+		if (i % 5 == 0) return "Buzz";
+		return i.ToString();
+	}
+}
---
+++ FizzBuzz/RockPaperScissors.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+
+[TestFixture]
+public class RockPaperScissorsTests
+{
+	[TestCase ("Rock", "Scissors", "Player1")]
+	[TestCase ("Paper", "Rock", "Player1")]
+	[TestCase ("Scissors", "Paper", "Player1")]
+	[TestCase ("Paper", "Scissors", "Player2")]
+	[TestCase ("Rock", "Rock", "Tie")]
+	public void WinCondition (string input1, string input2, string expected)
+	{
+		string result = RockPaperScissors.winCondition (input1, input2);
+		Assert.That (result, Is.EqualTo(expected));
+	}
+}
+
+public class RockPaperScissors
+{
+	public static string winCondition(string weapon1, string weapon2)
+	{
+		if (weapon1 == "Rock" && weapon2 == "Scissors")
+			return "Player1";
+		if (weapon1 == "Paper" && weapon2 == "Rock")
+			return "Player1";
+		if (weapon1 == "Scissors" && weapon2 == "Paper")
+			return "Player1";
+		if (weapon1 == "Paper" && weapon2 == "Scissors")
+			return "Player2";
+		if (weapon1 == "Rock" && weapon2 == "Rock")
+			return "Tie";
+		return "No Game";
+	}
+}
