fbd789ad913e2ced16564decf1818dd90a43c3f6
Nicholas Emanuel <njemanuel01@gmail.com>
2015-08-14 12:40:03 -0500

    Continued adding tests to changemaker

--- FizzBuzz/ChangeMaker.cs
+++ FizzBuzz/ChangeMaker.cs
@@ -9,6 +9,8 @@ public class ChangeMakerTests
 	[TestCase (1.25,new int[] {1, 1}, TestName="Test 2")]
 	[TestCase (1.35, new int[] {1, 1, 1}, TestName="Test 3")]
 	[TestCase (1.40, new int[] {1, 1, 1, 1}, TestName="Test 4")]
+	[TestCase (1.41, new int[] {1, 1, 1, 1, 1}, TestName="Test 5")]
+	[TestCase (1.69, new int[] {1, 2, 1, 1, 4}, TestName="Test 6")]
 	public void ChangeMaker(decimal input, int[] expected)
 	{
 		int[] result = Changer.MakeChange(input);
@@ -23,20 +25,57 @@ public class Changer
 		List<int> list = new List<int> ();
 		decimal amount = i * 100;
 		int int_amount = Convert.ToInt32 (amount);
-		if (int_amount / 100 > 0) {
-			list.Add (int_amount / 100);
+		if (Dollar(int_amount) > 0) 
+		{
+			list.Add (Dollar(int_amount));
 		}
 		int_amount = int_amount % 100;
-		if (int_amount / 25 > 0) 
+		if (Quarter(int_amount) > 0) 
 		{
-			list.Add (int_amount / 25);
+			list.Add (Quarter(int_amount));
 		}
 		int_amount = int_amount % 25;
-		if (int_amount / 10 > 0) {
-			list.Add (int_amount / 10);
+		if (Dime(int_amount) > 0) 
+		{
+			list.Add (Dime(int_amount));
+		}
+		int_amount = int_amount % 10;
+		if (Nickel(int_amount) > 0) 
+		{
+			list.Add (Nickel(int_amount));
+		}
+		int_amount = int_amount % 5;
+		if (Penny(int_amount) > 0)
+		{
+			list.Add (Penny(int_amount));
 		}
 
 		return  list.ToArray();
+	}
+
+	public static int Dollar(int amount)
+	{
+		return amount / 100;
+	}
+
+	public static int Quarter(int amount)
+	{
+		return amount / 25;
+	}
+
+	public static int Dime(int amount)
+	{
+		return amount / 10;
+	}
+
+	public static int Nickel(int amount)
+	{
+		return amount / 5;
+	}
+
+	public static int Penny(int amount)
+	{
+		return amount / 1;
 	}
 }
 
